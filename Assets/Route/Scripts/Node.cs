5c74e1fff79f36d3e12690c1479ee8b54d802a0f
Adam Passey <adamjpassey@gmail.com>
2019-04-29 22:37:51 -0700

    Node `OnDestroy` and `CustomDestroy` now send message outwards

--- Assets/Route/Scripts/Node.cs
+++ Assets/Route/Scripts/Node.cs
@@ -5,9 +5,15 @@ using System.Linq;
 
 namespace Route {
 
+    interface INodeEventListener {
+        void CustomDestroy(Node n);
+    }
+
     [ExecuteInEditMode]
     [AddComponentMenu("Route/Node")]
     public class Node : MonoBehaviour {
+
+        public static readonly string CUSTOM_DESTROY = "CustomDestroy";
 
         public int cost = 1;
 
@@ -35,12 +41,29 @@ namespace Route {
             }
         }
 
+        /// <summary>
+        /// Custom destroy called by editor when the backspace key is hit
+        /// when this object is selected in edit mode
+        /// </summary>
         public void CustomDestroy() {
+            //  break the prefab instance and notify all components of being custom destroyed
+            //  Inherit from INodeListener to receive this event
+            //  must be using [ExecuteInEditMode] to receive it during edit mode
+            PrefabUtility.UnpackPrefabInstance(gameObject, PrefabUnpackMode.Completely, InteractionMode.AutomatedAction);
+            SendMessage(CUSTOM_DESTROY, this, SendMessageOptions.DontRequireReceiver);
             foreach (Node n in neighbors) {
                 n.directNeighbors.Remove(this);
                 n.diagonalNeighbors.Remove(this);
             }
             DestroyImmediate(this);
+        }
+
+        public void OnDestroy() {
+            SendMessage(CUSTOM_DESTROY, this, SendMessageOptions.DontRequireReceiver);
+            foreach (Node n in neighbors) {
+                n.directNeighbors.Remove(this);
+                n.diagonalNeighbors.Remove(this);
+            }
         }
     }
 }
